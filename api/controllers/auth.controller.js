e25a87348d3f67c74a70d8b1affedb7a2d7a25a3
Bon1bon <bonim233@gmail.com>
2024-09-15 11:23:49 +0300

    signup route added

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signUp = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === "" || email === "" || password === "") {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    try {
+        await newUser.save();
+        res.json({ message: "Signup successfull" });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ message: err.message });
+    }
+};

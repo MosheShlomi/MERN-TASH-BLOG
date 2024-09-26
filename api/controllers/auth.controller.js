5956774f384c104655990cacd964ec3527359ca2
Bon1bon <bonim233@gmail.com>
2024-09-26 10:58:24 +0300

    error handlers added for signup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fields are required" });
+        next(errorHandler(400, "All fields are required"));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -15,7 +16,6 @@ export const signUp = async (req, res) => {
         await newUser.save();
         res.json({ message: "Signup successfull" });
     } catch (err) {
-        console.log(err);
-        return res.status(400).json({ message: err.message });
+        next(err);
     }
 };

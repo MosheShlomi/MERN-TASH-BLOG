a9d3a9a095a25945f90fa50553e95b03a000fb22
Bon1bon <bonim233@gmail.com>
2024-11-19 14:09:28 +0200

    update user api route added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -22,6 +23,8 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
 });
+
+app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);

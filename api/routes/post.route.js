90ccbf6821369fabb951a1eeb1a63eca4178ea3a
Bon1bon <bonim233@gmail.com>
2024-11-24 13:36:59 +0200

    create post functionallity added

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { verifyToken } from "../utils/verifyUser";
+import { verifyToken } from "../utils/verifyUser.js";
 import { create } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.post("/create-post", verifyToken, create);
+router.post("/create", verifyToken, create);
 
 export default router;

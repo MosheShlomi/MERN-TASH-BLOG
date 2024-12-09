83c7231cf3fccbe61b6ac062ae4298bab2c4a8dc
Bon1bon <bonim233@gmail.com>
2024-12-09 20:23:00 +0200

    about and projects pages are ready

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-3xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Tash Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+              harum, numquam sed in atque possimus corporis rerum dolore,
+              suscipit accusantium iste eaque fugiat amet ab quos non
+              repellendus facilis. Ex atque eveniet ducimus sed natus enim sint
+              labore assumenda. Doloremque autem ea sequi suscipit earum
+              nostrum, velit minima minus possimus cum? Hic laboriosam iste
+              laborum dignissimos ex exercitationem quae ad vitae quibusdam odit
+              excepturi id eius nulla sint repudiandae pariatur quam voluptatum,
+              esse quo aliquid quos provident tempora eos dolorum. Mollitia,
+              saepe doloremque deleniti corrupti fuga quae soluta? Deleniti
+              architecto omnis laboriosam magnam. Voluptatem, consequuntur.
+              Dolores possimus eaque similique adipisci quaerat libero deserunt
+              alias repellat error pariatur dolorum rem aliquam maxime, vel
+              placeat obcaecati sint sit ipsam, enim cupiditate minima et?
+              Repellat odio nobis, harum, qui dicta eos ipsa, impedit laudantium
+              reiciendis pariatur earum accusantium officia omnis et cupiditate
+              asperiores accusamus natus maiores id aspernatur doloribus iste
+              dolores sed velit? Est dignissimos laborum accusamus commodi
+              exercitationem maxime iure. Illum corrupti ullam quis temporibus
+              sapiente omnis tempore perspiciatis recusandae consequatur
+              veritatis cupiditate alias voluptatum quasi, ipsa itaque
+              doloremque rem ab unde ex? Corporis architecto magnam est
+              explicabo culpa assumenda voluptas sunt animi distinctio soluta
+              illo, odio aliquam dolore provident ratione quia?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
